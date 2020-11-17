967feabd7338b594c721eaf2cb9eab937114d857
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-17 01:03:35 -0600

    commit tree 0af3876be7e086fae8eb2c002b1cbd8b765a3144

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)  ## this program will tell who is in line and what order
+# i don't understand going from (array) to place
+place_in_line = []  
+    if array.empty?
+        puts "The line is currently empty."
+    else
+        counter = 1
+        array.each do |customer| 
+         place_in_line << "#{counter}. #{customer}"
+        counter += 1
+                    end
+  puts "The line is currently: #{place_in_line.join(" ")}"
+    end
+end
+
+def take_a_number(katz_deli, customer)
+    katz_deli.push(customer)
+puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."   
+end
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
+
